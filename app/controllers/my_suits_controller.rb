37fd207d9e0a0e10dbacdb2d89f97bf74b7cabe2
MaximuSSmile <MaximusGrandDeveloper@gmail.com>
2016-04-27 06:15:21 +0300

    start app

--- app/controllers/my_suits_controller.rb
+++ app/controllers/my_suits_controller.rb
@@ -2,8 +2,12 @@ class MySuitsController < ApplicationController
 		def welcome
 			@title = 'HELLO WORLD'
 		end
-		
+
 		def about_us
 			@title = 'I Will be Great Programmer'
+		end
+
+		def hello
+			@title = 'HELLO WORLD'
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'my_suits#welcome'
+  root 'my_suits#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
