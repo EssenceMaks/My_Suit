04e62a767bd51e7f1c70119fab7ff3deaba4dece
MaximuSSmile <MaximusGrandDeveloper@gmail.com>
2016-05-16 06:49:24 +0300

    horoku try 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 config.cache_classes = true
-config.serve_static_assets = true
+config.serve_static_files = true
 config.assets.compile = true
 config.assets.digest = true
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
