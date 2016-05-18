d554e6fe16fd9ff4f003d77268518715251a02cc
MaximuSSmile <MaximusGrandDeveloper@gmail.com>
2016-05-18 23:26:15 +0300

    new styles13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,14 +28,16 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 config.serve_static_files = true
-config.serve_static_files = true
 config.assets.initialize_on_precompile = false
+
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -84,4 +86,8 @@ config.assets.initialize_on_precompile = false
   config.active_record.dump_schema_after_migration = false
 end
 
-
+#config.cache_classes = true
+#config.serve_static_assets = true
+#config.assets.compile = true
+#config.assets.digest = true
+#rake assets:precompile  ввести для перезаписи стилей
